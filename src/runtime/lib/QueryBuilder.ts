a4ad7643186aad77a9fead3c9572b3e33788d27c
etienne1698 <etienne.robert1698@gmail.com>
2025-03-28 15:31:30 +0100

    update

--- src/runtime/lib/QueryBuilder.ts
+++ src/runtime/lib/QueryBuilder.ts
@@ -1,14 +1,22 @@
 import type Model from "./Model";
 import type Repository from "./Repository";
 
+export type OperatorValueType = {
+  $eq: any;
+  $in: Array<any>;
+  $ne: any;
+};
+
+export type Operator = keyof OperatorValueType;
+
 export default class QueryBuilder<M extends Model> {
   #repository!: Repository<M>;
   #withRelated = new Set<string>();
 
-  #filters = {
-    eq: {} as { [key: string]: any },
-    ne: {} as { [key: string]: any },
-    in: {} as { [key: string]: any },
+  #filters: Record<Operator, Record<string, any>> = {
+    $eq: {},
+    $in: {},
+    $ne: {},
   };
 
   constructor(repository: Repository<M>) {
@@ -20,19 +28,26 @@ export default class QueryBuilder<M extends Model> {
     return this;
   }
 
-  whereEq(field: string, value: any) {
-    this.#filters.eq[field] = value;
+  where<T extends keyof OperatorValueType>(
+    field: string,
+    op: T,
+    value: OperatorValueType[T]
+  ) {
+    // @ts-ignore
+    this.#filters[op][field] = value;
     return this;
+  }
+
+  whereEq(field: string, value: any) {
+    return this.where(field, "$eq", value);
   }
 
   whereNe(field: string, value: any) {
-    this.#filters.ne[field] = value;
-    return this;
+    return this.where(field, "$ne", value);
   }
 
-  whereIn(field: string, value: any) {
-    this.#filters.in[field] = value;
-    return this;
+  whereIn(field: string, value: Array<any>) {
+    return this.where(field, "$in", value);
   }
 
   get() {
