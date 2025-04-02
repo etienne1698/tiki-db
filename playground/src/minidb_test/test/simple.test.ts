035a406b240d7d980ab02b1de6af279b0690c876
etienne1698 <etienne.robert1698@gmail.com>
2025-04-02 16:23:52 +0200

    update

--- playground/src/minidb_test/test/simple.test.ts
+++ playground/src/minidb_test/test/simple.test.ts
@@ -1,5 +1,26 @@
 import { expect, test } from "vitest";
 
-test("simple save and retrieve", () => {
-  
+import { Database, hasMany, model, string } from "../index";
+
+test("test", () => {
+  const pet = model({
+    id: string("id", ""),
+    user_id: string("user_id", ""),
+  });
+
+  const user = model(
+    {
+      id: string("id", ""),
+    },
+    () => ({
+      pets: hasMany(pet, "user_id"),
+    })
+  );
+
+  const db = new Database({
+    user,
+    pet,
+  });
+
+  return expect(db.models.user.relations().pets.field).toBe("user_id");
 });
