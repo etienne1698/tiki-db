c13f9274f80d9d97f92b300ace290554ea4b85f5
etienne1698 <etienne.robert1698@gmail.com>
2025-04-02 12:20:14 +0200

    update

--- playground/src/minidb_test/minidb.ts
+++ playground/src/minidb_test/minidb.ts
@@ -11,7 +11,7 @@ export enum MapModelAction {
   UPDATE = 2,
 }
 
-export type Model<
+export type ModelSchema<
   T,
   Relations extends Record<string, Relation<unknown>> = {}
 > = {
@@ -21,22 +21,19 @@ export type Model<
   entity: string;
 };
 
+export type InferMappedModel<T extends ModelSchema<unknown>> = ReturnType<T["map"]>;
 
-export type InferMappedModel<T extends Model<unknown>> = ReturnType<T["map"]>;
+const defaultMap = <T>(data: any, _action: MapModelAction) => data as T;
 
 function model<T, Relations extends Record<string, Relation<unknown>> = {}>({
   primaryKey,
   entity,
   relations,
-}: Partial<Model<T, Relations>> & Pick<Model<T, Relations>, "entity">): Model<
-  T,
-  Relations
-> {
+  map,
+}: Partial<ModelSchema<T, Relations>> & Pick<ModelSchema<T, Relations>, "entity">) {
   return {
-    relations: relations || (() => ({} as Relations)),
-    map(data, _action: MapModelAction) {
-      return data as T;
-    },
+    relations: (relations || (() => ({} as Relations))) as () => Relations,
+    map: map || defaultMap,
     primaryKey: primaryKey || "id",
     entity,
   };
@@ -44,32 +41,35 @@ function model<T, Relations extends Record<string, Relation<unknown>> = {}>({
 
 // Relations
 
-export abstract class Relation<T, M extends Model<T> = Model<T>> {
+export abstract class Relation<T, M extends ModelSchema<T> = ModelSchema<T>> {
   constructor(public related: M, public field: string) {
     this.related = related;
     this.field = field;
   }
 
-  abstract getFor(data: any, database: Database<any>): InferMappedModel<M> | InferMappedModel<M>[];
+  abstract getFor(
+    data: any,
+    database: Database<any>
+  ): InferMappedModel<M> | InferMappedModel<M>[];
 }
 
-export class HasManyRelation<T, M extends Model<T>> extends Relation<T, M> {
-  override getFor(data: any, database: Database<any>): InferMappedModel<M>[] {
+export class HasManyRelation<T, M extends ModelSchema<T>> extends Relation<T, M> {
+  override getFor(_data: any, _database: Database<any>): InferMappedModel<M>[] {
     return [];
   }
 }
 
-export function hasMany<T, M extends Model<T>>(model: M, field: string) {
+export function hasMany<T, M extends ModelSchema<T>>(model: M, field: string) {
   return new HasManyRelation(model, field);
 }
 
 // Database
 
-class Database<Models extends Record<string, Model<unknown>>> {
+class Database<Models extends Record<string, ModelSchema<unknown>>> {
   constructor(public models: Models) {}
 }
 
-function createDatabase<Models extends Record<string, Model<unknown>>>(
+function createDatabase<Models extends Record<string, ModelSchema<unknown>>>(
   model: Models
 ): Database<Models> {
   return new Database<Models>(model);
@@ -88,13 +88,14 @@ const petSchema = model<{ user_id: string }>({
   entity: "pets",
 });
 
-
-
 const db = createDatabase({
   userSchema,
   petSchema,
 });
 
-db.models.userSchema.relations().pets.getFor({}, db).map(pet => {
-  console.log(pet.user_id); 
-});
+db.models.userSchema
+  .relations()
+  .pets.getFor({}, db)
+  .map((pet) => {
+    console.log(pet.user_id);
+  });
