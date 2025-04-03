a16d3584d3ad25f1f1854a7ae7e4b16a30e81dea
etienne1698 <etienne.robert1698@gmail.com>
2025-04-03 11:31:48 +0200

    update

--- playground/src/syn-orm/test.test.ts
+++ playground/src/syn-orm/test.test.ts
@@ -9,15 +9,29 @@ class Collection<
   constructor(public collectionName: CollectionName, public schema: S) {}
 }
 
-class Relation<CollectionName extends string = string> {
-  constructor(public referenceModelName: string, public field: string) {}
+class Relation<RefCName extends string = string> {
+  declare referencedCollectionName: RefCName;
+
+  constructor(
+    referencedCollection: Collection<RefCName>,
+    public field: string
+  ) {
+    this.referencedCollectionName = referencedCollection.collectionName;
+  }
 }
+
+class HasMany<RefCName extends string = string> extends Relation<RefCName> {}
+
+class BelongsTo<RefCName extends string = string> extends Relation<RefCName> {}
 
 class Relations<
   CollectionName extends string = string,
   Config extends Record<string, Relation> = Record<string, Relation>
 > {
-  constructor(public collectionName: CollectionName, public config: Config) {}
+  constructor(
+    public collection: Collection<CollectionName>,
+    public config: Config
+  ) {}
 }
 
 function collection<
@@ -27,8 +41,49 @@ function collection<
   return new Collection(collectionName, new Schema(schema));
 }
 
+function hasMany<RefC extends Collection>(
+  referencedCollection: RefC,
+  field: keyof RefC["schema"]["schema"]
+) {
+  return new HasMany(referencedCollection, field as string);
+}
+function belongsTo<C extends Collection, RefC extends Collection = Collection>(
+  referencedCollection: RefC,
+  field: keyof C["schema"]["schema"]
+) {
+  return new BelongsTo(referencedCollection, field as string);
+}
+
+type RelationSetupFn<
+  C extends Collection = Collection,
+  Config extends Record<string, Relation> = Record<string, Relation>
+> = (relations: {
+  hasMany: typeof hasMany;
+  belongsTo: typeof belongsTo<C>;
+}) => Config;
+
+function relations<
+  C extends Collection = Collection,
+  Config extends Record<string, Relation> = Record<string, Relation>
+>(collection: C, setup: RelationSetupFn<C, Config>) {
+  return new Relations(collection, setup({ hasMany, belongsTo }));
+}
+
 // --------
 
-const user = collection("users", {
+const users = collection("users", {
   id: string("id", ""),
 });
+
+const pets = collection("users", {
+  id: string("id", ""),
+  user_id: string("id", ""),
+});
+
+const usersRelations = relations(users, ({ hasMany }) => ({
+  pets: hasMany(pets, "user_id"),
+}));
+
+const petsRelations = relations(pets, ({ belongsTo }) => ({
+  user: belongsTo(users, "user_id"),
+}));
