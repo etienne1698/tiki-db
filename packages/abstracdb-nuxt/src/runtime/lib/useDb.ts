de04c5f49d95e5b9026628e4354b227d8f5687d1
etienne1698 <etienne.robert1698@gmail.com>
2025-04-04 17:45:05 +0200

    update

--- packages/abstracdb-nuxt/src/runtime/lib/useDb.ts
+++
@@ -1,7 +0,0 @@
-import NuxtDatabase from "./NuxtDatabase";
-import { useRuntimeConfig } from "#app";
-
-export default function useDb() {
-  const prefix = useRuntimeConfig().public.dbPrefix as string;
-  return NuxtDatabase.createWithPrefix(prefix);
-}
