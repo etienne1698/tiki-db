9eca45d1d2162c016c3954058cab3d3d2e342930
etienne1698 <etienne.robert1698@gmail.com>
2025-04-28 01:22:36 +0200

    update

--- packages/tiki-db-author-cli/src/index.ts
+++ packages/tiki-db-author-cli/src/index.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 import * as prompts from "@inquirer/prompts";
-import fs from "fs/promises";
+import fs from "fs";
 import path from "path";
 import { toSnakeCase } from "./utils";
 import { downloadFileContent, listFilesRecursively } from "./github";
@@ -46,11 +46,25 @@ async function main() {
     inRepoTestsPath
   );
 
+  const testBaseFilePath = path.resolve(dirPath, "base.ts");
+  if (fs.existsSync(testBaseFilePath)) {
+    const overwriteTestBase = await prompts.confirm({
+      message:
+        "Test base (base.ts) already exists, would you like to overwrite it ?",
+    });
+    if (!overwriteTestBase) {
+      allTemplatesPaths.splice(
+        allTemplatesPaths.findIndex((p) => p.includes("base.ts")),
+        1
+      );
+    }
+  }
+
   for (const templatePath of allTemplatesPaths) {
     const fileName = templatePath.replace(`${inRepoTestsPath}/`, "");
     const filePath = path.resolve(dirPath, fileName);
-    await fs.mkdir(path.dirname(filePath), { recursive: true });
-    await fs.writeFile(
+    fs.mkdirSync(path.dirname(filePath), { recursive: true });
+    fs.writeFileSync(
       filePath,
       updateTestTemplateContent(
         await downloadFileContent("etienne1698", "tiki-db", templatePath),
