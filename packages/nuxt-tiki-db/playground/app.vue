be471d074550c13ea6d9a0f4b817cf57388b4bf7
etienne1698 <etienne.robert1698@gmail.com>
2025-05-04 12:01:19 +0200

    update

--- packages/nuxt-tiki-db/playground/app.vue
+++ packages/nuxt-tiki-db/playground/app.vue
@@ -1,6 +1,7 @@
 <template>
-  <div>
-    <div>{{ users }}</div>
+  <div v-for="user of users" :key="user.id!">
+    <pre>{{ user.id }}</pre>
+    <button @click="removeUser(user.id!)">DELETE</button>
   </div>
 </template>
 
@@ -10,7 +11,17 @@ import { seed } from "./data/seed";
 
 const database = useDB(db);
 database.init();
-seed(database);
+if (import.meta.server) {
+  seed(database);
+}
 
 const users = computed(() => database.collections.users.findMany({}).value);
+
+function removeUser(id: string) {
+  database.collections.users.remove({
+    id: {
+      $eq: id,
+    },
+  });
+}
 </script>
--- packages/nuxt-tiki-db/src/runtime/plugins/queries_manager_plugin.ts
+++ packages/nuxt-tiki-db/src/runtime/plugins/queries_manager_plugin.ts
@@ -1,4 +1,4 @@
-import { QueriesManager } from "tiki-db";
+import { QueriesManager, type QueryCacheData } from "tiki-db";
 import {
   definePayloadPlugin,
   definePayloadReducer,
@@ -8,9 +8,31 @@ import {
 const PAYLOAD_TYPE = "Model";
 
 export default definePayloadPlugin((_nuxtApp) => {
-  definePayloadReducer(
-    PAYLOAD_TYPE,
-    (data) => data instanceof QueriesManager && JSON.stringify({})
-  );
-  definePayloadReviver(PAYLOAD_TYPE, (data) => ({}));
+  definePayloadReducer(PAYLOAD_TYPE, (data) => {
+    const dataIsQueriesManager = data instanceof QueriesManager;
+    if (!dataIsQueriesManager) return false;
+    const toStringify: {
+      [key: string]: { [key in keyof QueryCacheData<Ref>]: any };
+    } = {};
+    for (const [queryHash, qc] of Object.entries(data.queries) as [
+      string,
+      QueryCacheData<Ref>
+    ][]) {
+      toStringify[queryHash] = qc;
+      toStringify[queryHash].result = toStringify[queryHash].result.value;
+    }
+    return JSON.stringify(toStringify);
+  });
+  definePayloadReviver(PAYLOAD_TYPE, (data) => {
+    const qm = new QueriesManager<Ref>();
+
+    for (const [queryHash, qc] of Object.entries(JSON.parse(data)) as [
+      string,
+      QueryCacheData<Ref>
+    ][]) {
+      qm.queries[queryHash] = qc;
+      qm.queries[queryHash].result = ref(qc.result);
+    }
+    return qm;
+  });
 });
