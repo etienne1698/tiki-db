020e7835774c2169e85ea55aeeed9f82bf51f569
etienne1698 <etienne.robert1698@gmail.com>
2025-05-15 11:55:07 +0200

    update tests idb

--- packages/tiki-db-idb/test/idb/base.ts
+++ packages/tiki-db-idb/test/idb/base.ts
@@ -1,59 +1,8 @@
-import { collection, model, relations, string, asyncDatabase } from "tiki-db";
+import { asyncDatabase } from "tiki-db";
 import { getTestStorage } from "./base_storage";
+import { collections } from "./base_schema";
 
 export async function getTestDatabase() {
-  const users = model("usersDbName", {
-    id: string("identifier", ""),
-    firstname: string("firstname", ""),
-    lastname: string("lastname", ""),
-    email: string("email", ""),
-    phone: string("phone", ""),
-  });
-  const posts = model("postsDbName", {
-    id: string("id", ""),
-    title: string("title", ""),
-    content: string("content", ""),
-    userId: string("userId", "").notNull(),
-  });
-
-  const comments = model("commentsDbName", {
-    id: string("id", ""),
-    title: string("title", ""),
-    content: string("content", ""),
-    postId: string("postId", "").notNull(),
-  });
-
-  const usersRelations = relations(users, ({ many }) => ({
-    relatedPosts: many(posts, {
-      fields: ["id"],
-      references: ["userId"],
-    }),
-  }));
-
-  const postsRelations = relations(posts, ({ one, many }) => ({
-    relatedUser: one(users, {
-      fields: ["userId"],
-      references: ["id"],
-    }),
-    relatedComments: many(comments, {
-      fields: ["id"],
-      references: ["postId"],
-    }),
-  }));
-
-  const commentsRelations = relations(comments, ({ one }) => ({
-    relatedPost: one(posts, {
-      fields: ["postId"],
-      references: ["id"],
-    }),
-  }));
-
-  const collections = {
-    users: collection(users, usersRelations),
-    posts: collection(posts, postsRelations),
-    comments: collection(comments, commentsRelations),
-  };
-
   const storage = await getTestStorage();
 
   const db = asyncDatabase(collections, storage);
--- packages/tiki-db-idb/test/idb/base_storage.ts
+++ packages/tiki-db-idb/test/idb/base_storage.ts
@@ -1,6 +1,8 @@
 import "fake-indexeddb/auto";
 import { IndexedDBStorage } from "../../src";
+import { collections } from "./base_schema";
+import { DatabaseFullSchema } from "tiki-db";
 
 export async function getTestStorage() {
-  return new IndexedDBStorage({ dbName: "myDB" });
+  return new IndexedDBStorage<DatabaseFullSchema<typeof collections>>({ dbName: "myDB" });
 }
