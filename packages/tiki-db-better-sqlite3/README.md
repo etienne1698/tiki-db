ea862e23e42d8f52a1b8babe218c2aed1d651b8d
etienne1698 <etienne.robert1698@gmail.com>
2025-05-23 00:23:38 +0200

    delete better-sqlite3

--- packages/tiki-db-better-sqlite3/README.md
+++
@@ -1,3 +0,0 @@
-Found the README for tiki-db at:
-
-[README.md](https://github.com/etienne1698/tiki-db)
--- packages/tiki-db-better-sqlite3/test/sqlite/base_storage.ts
+++
@@ -1,6 +0,0 @@
-import "fake-indexeddb/auto";
-import { BetterSqlite3Storage } from "../../src";
-
-export function getTestStorage() {
-  return new BetterSqlite3Storage();
-}
