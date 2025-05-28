6a8e2f3a1556e5ec6b9ba274b71973d741840555
etienne1698 <etienne.robert1698@gmail.com>
2025-05-28 17:16:00 +0200

    update example

--- examples/vue_inmemory/src/App.vue
+++ examples/vue_inmemory/src/App.vue
@@ -1,32 +1,6 @@
 <script setup lang="ts">
-import { computed, ref } from "vue";
-import { db } from "./data/database";
-import Button from "./components/Button.vue";
-import Accordion from "./components/Accordion.vue";
 import Layout from "./components/Layout.vue";
 import { RouterView } from "vue-router";
-
-const users = computed(
-  () => db.collections.users.findMany({ with: { posts: true } }).value
-);
-
-const userColors = ref(new Map<string, string>());
-
-function getRandomColor() {
-  return `hsl(${Math.random() * 360}, 70%, 80%)`;
-}
-
-function getUserColor(id: string) {
-  if (!userColors.value.has(id)) {
-    userColors.value.set(id, getRandomColor());
-  }
-  return userColors.value.get(id);
-}
-
-function removeUser(id: string) {
-  db.collections.users.remove({ id: { $eq: id } });
-  userColors.value.delete(id);
-}
 </script>
 
 <template>
--- examples/vue_inmemory/src/router/index.ts
+++ examples/vue_inmemory/src/router/index.ts
@@ -1,21 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import HomeView from '../views/HomeView.vue';
-import PostsView from '../views/PostsView.vue';
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+import PostsView from "../views/PostsView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: HomeView,
     },
     {
-      path: '/posts',
-      name: 'posts',
-      component: PostsView
-    }
-  ]
+      path: "/posts",
+      name: "posts",
+      component: PostsView,
+    },
+  ],
 });
 
-export default router; 
+export default router;
